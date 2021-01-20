a1fdd3901f0c713178848ba1baf7e567439cc5bb
KiriqN <57619714+KiriqN@users.noreply.github.com>
2021-01-20 14:42:26 +0200

    Add files via upload

---
+++ GADE_POE/Goblin.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE_POE
+{
+    [Serializable]
+    class Goblin : Enemy
+    {
+        public Goblin(int _posx, int _posy, string _symbol = "G", int _damage = 1, int _hp = 10):base(_posx, _posy, _symbol, _damage, _hp)
+        {
+            base.goldpurse = 1;
+            base.weapon = new MeleeWeapon(WeaponType.Dagger, "D");
+            
+        }
+
+        public override movement ReturnMove(movement move = 0)
+        {
+            int randomdir = r.Next(0, 4);
+            move = movement.NoMovement;
+
+            while (move == movement.NoMovement)
+            {
+                if (randomdir == 0)
+                {
+                    if (CHARACTERVISION[0] is EmptyTile || CHARACTERVISION[0] is Item)
+                    {
+                        if (CHARACTERVISION[0] is Item)
+                        {
+                            PickUp(CHARACTERVISION[0] as Item);
+                        }
+                        move = movement.Left;
+                        POSX--;
+                    }
+                    else
+                    {
+                        randomdir = r.Next(0, 4);
+                    }
+                }
+                else if (randomdir == 1)
+                {
+                    if (CHARACTERVISION[1] is EmptyTile || CHARACTERVISION[1] is Item)
+                    {
+                        if (CHARACTERVISION[1] is Item)
+                        {
+                            PickUp(CHARACTERVISION[1] as Item);
+                        }
+                        move = movement.Up;
+                        POSY--;
+                    }
+                    else
+                    {
+                        randomdir = r.Next(0, 4);
+                    }
+                }
+                else if (randomdir == 2)
+                {
+                    if (CHARACTERVISION[2] is EmptyTile || CHARACTERVISION[2] is Item)
+                    {
+                        if (CHARACTERVISION[2] is Item)
+                        {
+                            PickUp(CHARACTERVISION[2] as Item);
+                        }
+                        move = movement.Right;
+                        POSX++;
+                    }
+                    else
+                    {
+                        randomdir = r.Next(0, 4);
+                    }
+                }
+                else if (randomdir == 3)
+                {
+                    if (CHARACTERVISION[3] is EmptyTile || CHARACTERVISION[3] is Item)
+                    {
+                        if (CHARACTERVISION[3] is Item)
+                        {
+                            PickUp(CHARACTERVISION[3] as Item);
+                        }
+                        move = movement.Down;
+                        POSY++;
+                    }
+                    else
+                    {
+                        randomdir = r.Next(0, 4);
+                    }
+                }
+                else
+                {
+                    move = movement.NoMovement;
+                }
+            }
+            return move;
+        }
+        //goblin tostring
+        public override string ToString()
+        {
+            
+            string output;
+            if (weapon == null)
+            {
+                output = "Bare Handed goblin at [" + POSX + "," + POSY + "] Deals:(" + DAMAGE + ") HP:" + HP + "Has " + GOLDPURSE + "gold";
+            }
+            else
+            {
+                output = "Goblin at [" + POSX + "," + POSY + "] Deals:(" + DAMAGE + ") HP:" + HP+ "Equiped with "+weapon.TYPE.ToString() + "Has " + GOLDPURSE + "gold";
+            }
+
+            return output;
+        }
+    }
+}
---
+++ GADE_POE/Map.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE_POE
+{
+    [Serializable]
+    class Map
+    {
+        private int mapwidth;
+        private int mapheight;
+        private int enemyamount;
+
+        public int MapWidth
+        {
+            get { return mapwidth; }
+            set { mapwidth = value; }
+        }
+        public int MapHeight
+        {
+            get { return mapheight; }
+            set { mapheight = value; }
+        }
+        public int EnemyAmount
+        {
+            get { return enemyamount; }
+            set { enemyamount = value; }
+        }
+        public Hero PLAYER
+        {
+            get { return player; }
+            set { player = value; }
+        }
+
+        private Hero player;
+        public Item[] items;
+        public Tile[,] map;
+        public Enemy[] enemies;
+        public Random r = new Random();
+        //contructor
+        public Map(int minmapwidth,int maxmapwidth,int minmapheight, int maxmapheight, int eenemyamount,  int itemamount)
+        {
+            this.MapWidth = r.Next(minmapwidth,maxmapwidth);
+            this.MapHeight = r.Next(minmapheight, maxmapheight);
+            this.EnemyAmount = eenemyamount;
+
+            items = new Item[itemamount];
+            map = new Tile[MapWidth, MapHeight];
+            enemies = new Enemy[EnemyAmount];
+
+            FillMap();
+            InitializeMap();
+
+            Tile tempHero = Create(TileType.Hero);
+            map[player.POSX, player.POSY] = tempHero;
+
+
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                enemies[i] = (Enemy)Create(TileType.Enemy);
+                map[enemies[i].POSX, enemies[i].POSY]= enemies[i];
+            }
+            for (int i = 0; i < items.Length; i++)
+            {
+                int temp = r.Next(1, 3);
+                if (temp == 1)
+                {
+                    items[i] = (Item)Create(TileType.Gold);
+                }
+                if (temp == 2)
+                {
+                    items[i] = (Item)Create(TileType.Weapon);
+                }
+                map[items[i].POSX, items[i].POSY] = items[i];
+            }
+
+            UpdateVision();
+        }
+        //methods
+        private Tile Create(TileType tiletype)
+        {
+            int randomX = r.Next(0, MapWidth);
+            int randomY = r.Next(0, MapHeight);
+
+            while (map[randomX, randomY] is Obstacle || map[randomX, randomY] is Character || map[randomX, randomY] is Item)
+            {
+                randomX = r.Next(0, MapWidth);
+                randomY = r.Next(0, MapHeight);
+            }
+            if (tiletype == TileType.Hero)
+            {
+                player = new Hero(randomX, randomY, 100);//player hp assigned here!
+                return player;
+            }
+            //added leader spawns
+            else if(tiletype == TileType.Enemy)
+            {
+                int temp = r.Next(1, 4);
+                if (temp == 1)
+                {
+                    return new Goblin(randomX, randomY);
+                }
+                else if (temp == 2)
+                {
+                    return new Mage(randomX, randomY);
+                }
+                else
+                {
+                    return new Leader(randomX, randomY, player);
+                }
+            }
+            //spawning weapons
+            else if (tiletype == TileType.Weapon)
+            {
+                int temp = r.Next(1, 5);
+                if (temp == 1)
+                {
+                    return new MeleeWeapon(WeaponType.Dagger, "D", randomX, randomY);
+                }
+                else if (temp == 2)
+                {
+                    return new MeleeWeapon(WeaponType.LongSword, "7", randomX, randomY);
+                }
+                else if(temp == 3)
+                {
+                    return new RangedWeapon(WeaponType.Rifle, "R", randomX, randomY);
+                }
+                else 
+                {
+                    return new RangedWeapon(WeaponType.Longbow, "L", randomX, randomY);
+                }
+            }
+            else 
+            {
+                return new Gold(randomX, randomY);
+            }
+            
+        }        
+        public void UpdateVision()
+        {
+            player.CHARACTERVISION[0] = map[player.POSX - 1, player.POSY];
+            player.CHARACTERVISION[1] = map[player.POSX , player.POSY - 1];
+            player.CHARACTERVISION[2] = map[player.POSX + 1, player.POSY]; 
+            player.CHARACTERVISION[3] = map[player.POSX , player.POSY + 1];
+
+            foreach (Enemy e in enemies)
+            {
+                e.CHARACTERVISION[0] = map[e.POSX - 1, e.POSY];
+                e.CHARACTERVISION[1] = map[e.POSX , e.POSY - 1];
+                e.CHARACTERVISION[2] = map[e.POSX + 1, e.POSY];
+                e.CHARACTERVISION[3] = map[e.POSX , e.POSY + 1];
+            }
+        }
+        public void InitializeMap()
+        {
+            for (int x = 0; x < MapWidth; x++)
+            {
+                for (int y = 0; y < MapHeight; y++)
+                {
+                    map[x, y] = new EmptyTile(x,y);
+                }
+            }
+            SpawnObstacles();
+        }
+        public void UpdateMap()
+        {
+            InitializeMap();
+            map[player.POSX, player.POSY] = player;
+
+            foreach (Enemy e in enemies)
+            {
+                if (e.IsDead() == false)
+                {
+                    map[e.POSX, e.POSY] = e;
+                }
+            }
+            foreach (Item gold in items)
+            {
+                if (gold != null)
+                {
+                    map[gold.POSX, gold.POSY] = gold;
+                }                
+            }
+        }
+        public void SpawnObstacles()
+        {
+            for (int x = 0; x < MapWidth; x++)
+            {
+                for (int y = 0; y < MapHeight; y++)
+                {
+                    if (x==0 || y ==0 || x== MapWidth - 1|| y == MapHeight - 1)
+                    {
+                        map[x, y] = new Obstacle(x ,y , "X");
+                    }
+                }
+            }
+        }
+        public void FillMap()
+        {
+            for (int x = 0; x < MapWidth; x++)
+            {
+                for (int y = 0; y < MapHeight; y++)
+                {
+                    map[x, y] = new EmptyTile(x,y);
+                }
+            }
+        }
+        public Item GetItemAtPosition(int x, int y)
+        {
+            Item pickedupitem = null;
+            for (int j = 0; j < items.Length; j++)
+            {
+                if (items[j] != null)
+                {
+                    if (items[j].POSX == x && items[j].POSY == y)
+                    {
+                        pickedupitem = items[j];
+                        items[j] = null;
+                    }
+                }
+            }
+            return pickedupitem;
+        }
+    }
+}
